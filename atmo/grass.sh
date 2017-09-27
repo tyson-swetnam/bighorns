921f6cbb0d46bc41db0b81d13e994ce77f49dcbc
Tyson L. Swetnam <tswetnam@cyverse.org>
2017-09-27 08:31:12 -0700

    Update grass.sh

--- atmo/grass.sh
+++ atmo/grass.sh
@@ -1,33 +1,110 @@
 #!/bin/bash
 
-# Build GRASS 7.2.2 from source
+#post
+echo "deb http://us.archive.ubuntu.com/ubuntu/ xenial main restricted universe multiverse" >> /etc/apt/sources.list
+
+sudo apt-get update && sudo apt-get install -y --no-install-recommends \
+bison \
+build-essential \
+ccache \
+checkinstall \
+cmake \
+curl \
+ffmpeg2theora \
+flex \
+g++ \
+gcc \
+gettext \
+ghostscript \
+libavcodec-dev \
+libavformat-dev \
+libav-tools \
+libavutil-dev \
+libboost-program-options-dev \
+libboost-thread-dev \
+libcairo2 \
+libcairo2-dev \
+libffmpegthumbnailer-dev \
+libfftw3-3 \
+libfftw3-dev \
+libfreetype6-dev \
+libgcc1 \
+libglu1-mesa-dev \
+libgsl-dev \
+libgtk2.0-dev \
+libgtkmm-3.0-dev \
+libjasper-dev \
+liblas-c-dev \
+libncurses5-dev \
+libnetcdf-dev \
+libperl-dev \
+libpng12-dev \
+libpnglite-dev \
+libpq-dev \
+libproj-dev \
+libreadline6 \
+libreadline6-dev \
+libsqlite3-dev \
+libswscale-dev \
+libtiff5-dev \
+libwxbase3.0-dev   \
+libwxgtk3.0-dev \
+libxmu-dev \
+libxmu-dev \
+libzmq3-dev \
+netcdf-bin \
+openjdk-8-jdk \
+pkg-config \
+proj-bin \
+proj-data \
+python \
+python-dateutil \
+python-dev \
+python-numpy \
+python-opengl \
+python-wxgtk3.0 \
+python-wxtools \
+python-wxversion \
+rsync \
+sqlite3 \
+subversion \
+swig \
+unzip \
+vim \
+wget \
+wx3.0-headers \
+wx-common \
+zlib1g-dev \
+
+sudo make -f gis_dependency.makefile
+
 cd /opt
-wget https://grass.osgeo.org/grass72/source/grass-7.2.2.tar.gz
-tar xzf grass-7.2.2.tar.gz
+sudo mkdir gis
+cd gis
+
+echo "Updating library paths"
+cd /etc/ld.so.conf.d
+echo "/opt/gis/lib" >> gis.conf
+echo "/opt/gis/lib64" >> gis.conf
+echo "/opt/gis/grass-7.2.2/lib" >grass.conf
+ldconfig
+
+# Build GRASS 7.2.2 from source
+sudo wget https://grass.osgeo.org/grass72/source/grass-7.2.2.tar.gz
+sudo tar xzf grass-7.2.2.tar.gz
 cd grass-7.2.2/
 
 # install any additional build dependency packages:
 sudo apt-get build-dep grass
 
-# configure to taste..
-CFLAGS="-O2 -Wall" LDFLAGS="-s" sudo ./configure \
-    --with-openmp \
-    --enable-largefile=yes \
-    --with-nls \
-    --with-cxx \
-    --with-proj-share=/usr/share/proj/ \
-    --with-geos \
-    --with-wxwidgets \
-    --with-cairo \
-    --with-opengl-libs=/usr/include/GL \
-    --with-freetype=yes --with-freetype-includes="/usr/include/freetype2/" \
-    --with-postgres=yes --with-postgres-includes="/usr/include/postgresql" \
-    --with-sqlite=yes \
-    --with-mysql=yes --with-mysql-includes="/usr/include/mysql" \
-    --with-odbc=no \
-     2>&1 | tee config_log.txt
+# environment
+GISBASE=/opt/gis/grass-7.2.2
+GRASS_PROJSHARE=/usr/share/proj
+LD_LIBRARY_PATH=/opt/gis/lib:/opt/gis/grass-7.2.2/lib
+PATH=/opt/gis/bin:/opt/gis/grass-7.2.2/bin:$PATH
+PYTHONPATH=/opt/gis/lib/python2.7/site-packages
+export GISBASE GRASS_PROJSHARE LD_LIBRARY_PATH PATH PYTHONPATH
 
-# build using 4 CPU cores
-time sudo make -j 4 2>&1 | tee build_log.txt
+# build info
+echo "Timestamp:" `date --utc` | tee /image-build-info.txt
 
-sudo make install

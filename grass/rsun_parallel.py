426f574f841ec7aec476a2edf1ca677d1b5a16d2
Tyson L. Swetnam <tswetnam@cyverse.org>
2017-09-27 21:10:58 -0700

    Update rsun_parallel.py

--- grass/rsun_parallel.py
+++ grass/rsun_parallel.py
@@ -7,7 +7,7 @@ def main(elevation):
     # compute slope and aspect for r.sun
     Module('r.slope.aspect', elevation=elevation, aspect='aspect', slope='slope', overwrite=True)
     # initialize an empty queue and list
-    queue = ParallelModuleQueue(nprocs=44)
+    queue = ParallelModuleQueue(nprocs=16)
     sun_name = 'sun_day{}_t{}'
     # set computational region
     Module('g.region', raster=elevation)
@@ -15,18 +15,17 @@ def main(elevation):
     for t in range(5,20):
         for d in range(1,366):
             print(d)
-            sun = Module('r.sun', elevation=elevation, slope='slope', aspect='aspect',
-                         beam_rad='beam', time=t, step=1, day=d, run_=False, overwrite=True)
+            sun = Module('r.sun.mp', elevation=elevation, slope='slope', aspect='aspect',
+                         glob_rad='glob', time=t, step=1, day=d, threads=16, run_=False, overwrite=True)
                 # create a copy of the module and set the remaining parameters
                 
-            m = deepcopy(sun)(beam_rad=sun_name.format(d,t), time=t)
+            m = deepcopy(sun)(glob_rad=sun_name.format(d,t), time=t)
             queue.put(m)
         queue.wait()
-#    Module('r.gdal.out', input=[sun_name.format(d,t) for t in range(5, 20)], 
-#           output='/mnt/Archive/goldwater/[sun_name.format(d,t) for t in range(5, 20)]', format='Gtiff', overwrite=True)
+    Module('r.gdal.out', createopt="COMPRESS=LZW", input=[sun_name.format(d,t) for t in range(5, 20)], 
+           output='/mnt/tyson_swetnam/Data/goldwater/[sun_name.format(d,t) for t in range(5, 20)]', format='Gtiff', overwrite=True)
     # set color table
     Module('r.colors', map=[sun_name.format(d,t) for t in range(5, 20)], color='grey', flags='e')
-
 if __name__ == '__main__':
     elevation = 'dem'
     main(elevation)

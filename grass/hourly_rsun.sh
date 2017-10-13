6dfa552adb38ccf42133b971bacd513714d76f2b
Tyson L. Swetnam <tswetnam@cyverse.org>
2017-10-12 20:56:54 -0700

    Update hourly_rsun.sh

--- grass/hourly_rsun.sh
+++ grass/hourly_rsun.sh
@@ -13,7 +13,7 @@ r.slope.aspect elevation=elev slope=slp aspect=asp
 # Calculate r.sun with openmp
 # loop for a month (test)
 # loop for daylight hours 5AM to 9PM
-for day in {1..31}
+for day in {352..366}
   do for hour in {5..21}
      do 
         r.sun.mp --overwrite --verbose elevation=elev aspect=asp slope=slp time=${hour} day=${day} beam_rad=beam_rad_${hour}_day${day} glob_rad=glob_rad_${hour}_day${day} threads=44

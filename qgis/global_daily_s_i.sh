af17c3428808d0cf719ffd715eabdbb1cf8fe126
Tyson L. Swetnam <tyson-swetnam@users.noreply.github.com>
2018-05-31 10:37:51 -0600

    Create global_daily_s_i.sh

---
+++ qgis/global_daily_s_i.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+ 
+# sudo singularity shell --bind /vol_c /vol_c/eemt-current
+# apt-get install python-gdal
+
+for ((i=1;i<=366;i++)); 
+do
+    echo -e "Calculating S_i for day $i"
+    gdal_calc.py -A "/vol_c/work/global/daily/total_sun_day_$i.tif" -B "/vol_c/work/global/daily/flat_total_sun_day_$i.tif" --outfile="/vol_c/s_i/s_i_day_$i.tif" --calc="A/B"
+done
